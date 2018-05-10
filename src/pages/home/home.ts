3eb6544b27735f1e29624ed2337ac965a3128920
Alejandro <alejito.matallana@gmail.com>
2018-05-10 10:53:13 -0500

    Comit inicial de la aplicacion del perfil con foto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	imagen: string;
+	constructor(
+		public navCtrl: NavController,
+		public cameraPlugin: Camera,
+		public alertCtrl: AlertController
+	) {}
 
-  constructor(public navCtrl: NavController) {
+	takePicture(): void {
+		const options: CameraOptions = {
+			quality: 100,
+			targetHeight: 1000,
+			targetWidth: 1000,
+			destinationType: this.cameraPlugin.DestinationType.DATA_URL,
+			encodingType: this.cameraPlugin.EncodingType.JPEG,
+			mediaType: this.cameraPlugin.MediaType.PICTURE
+		};
 
-  }
+		this.cameraPlugin.getPicture(options).then(
+			(imageData) => {
+				// imageData is either a base64 encoded string or a file URI
+				// If it's base64:
+				this.imagen = 'data:image/jpeg;base64,' + imageData;
+				console.log(this.imagen);
+			},
+			(err) => {
+				// Handle error
+			}
+		);
+	}
 
+	registrar(): void {
+		let alert = this.alertCtrl.create({
+			title: 'Exitoso',
+			subTitle: 'Has quedado registrado',
+			buttons: [ 'OK' ]
+		});
+		alert.present();
+	}
+
+	get validacion(): boolean {
+		if (this.imagen == '') {
+			return true;
+		}
+		return false;
+	}
 }
